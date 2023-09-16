c4d667047695919469cfbe753e8935a8dd462ed2
David Imo <134084030+R2codes@users.noreply.github.com>
2023-09-16 10:51:19 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
-
 import Body from "./body";
-import MovieId from './movieId'
+import MovieId from "./movieId";
 
-
-import { createBrowserRouter,
-Route,
-createRoutesFromElements,
-RouterProvider
-} from 'react-router-dom'
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
-
-  
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-          <Route index element={<Body />} />
+        <Route index element={<Body />} />
         <Route path="movies/:id" element={<MovieId />} />
-       
-     
       </Route>
-    
     )
   );
- 
 
- 
- 
   return (
-     
     <>
-    
-   <RouterProvider router={router} />
-    
+      <RouterProvider router={router} />
     </>
-
-  )
-   
- 
+  );
 }
 
 export default App;
